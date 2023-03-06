8d2fb43bfe4311bbd97a48b599cce19aef6f86c1
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2023-03-06 18:03:38 -0300

    added tests

--- api/authApp/models.py
+++ api/authApp/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     """
     Model for store user data
     """
-    name = models.CharField(max_length=120, null=True)
-    email = models.CharField(max_length=120, unique = True)
-    password = models.CharField(max_length=80)
-    birthdate = models.DateField(null=True)
+    name = models.CharField(max_length=120, null=False)
+    email = models.CharField(max_length=120, unique=True, null=False)
+    password = models.CharField(max_length=80, null=False)
+    birthdate = models.DateField(null=False)
--- api/authApp/tests.py
+++ api/authApp/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class ModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Set a data for testing it on the DB
+        """
+        cls.model = User.objects.create(name='testname', password=123456, email='test@test.com', birthdate='2000-1-1')
+
+    def test_model_data(self):
+        """
+        Test that the data was saved correctly in DB
+        """
+        self.assertEqual(self.model.name, 'testname')
+        self.assertEqual(self.model.password, 123456)
+        self.assertEqual(self.model.email, 'test@test.com')
+        self.assertEqual(self.model.birthdate, '2000-1-1')
+
+    def test_model_validation(self):
+        """
+        Test that the model's validation works as expected. It must fail
+        """
+        # This model has no birthdate, and birthdate is a not null field on User model
+        invalid_model = User(name='', password=123, email='')
+        with self.assertRaises(ValueError):
+            invalid_model.full_clean()
+
+    def test_model_delete(self):
+        # Test that the model can be deleted
+        self.model.delete()
+        self.assertFalse(User.objects.filter(name='testname').exists())
+
